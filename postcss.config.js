b257ae74c2d6206201d08612130d51f5d093cddc
die-go-dev <diego.rodrigues0308@gmail.com>
2024-06-07 11:54:40 -0700

    changes to color name and order page

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
     plugins: [
         require("tailwindcss/nesting"),
         require("tailwindcss"),
+        require("tailwindcss-animated"),
         require("autoprefixer"),
         ...(process.env.NODE_ENV === "production"
             ? [
