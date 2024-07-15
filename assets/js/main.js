a17435a714ed26b575be4dc01c68db2b330db591
die-go-dev <diego.rodrigues0308@gmail.com>
2024-07-15 10:25:57 -0700

    changes to styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,17 @@
 window.addEventListener("scroll", function () {
   const navbar = document.querySelector("#navbar");
-  if (this.window.scrollY > 300) {
+  const navbarLogo = this.document.querySelector(".navbar-logo");
+  if (this.window.scrollY > 230) {
     navbar.style.backgroundColor = "rgba(244, 151, 108, 1)";
     navbar.style.transition = "background-color 0.2s ease-in";
+    navbarLogo.classList.remove("hidden");
+    heroLogo.classList.add("hidden");
     // navbar.classList.add('scrolled');
   } else {
     navbar.style.backgroundColor = "transparent";
+    navbar.style.transition = "background-color 0.2s ease-out";
+    navbarLogo.classList.add("hidden");
+
     // navbar.classList.remove('scrolled');
   }
 });
